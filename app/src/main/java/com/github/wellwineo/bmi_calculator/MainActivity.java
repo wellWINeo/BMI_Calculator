c6f11f523c164d2f3a0d1eb91189588790830087
wellWINeo <usa200313@gmail.com>
2021-12-12 20:45:52 +0300

    add layouts for all fragments

--- app/src/main/java/com/github/wellwineo/bmi_calculator/MainActivity.java
+++ app/src/main/java/com/github/wellwineo/bmi_calculator/MainActivity.java
@@ -1,19 +1,15 @@
 package com.github.wellwineo.bmi_calculator;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceManager;
-import android.view.MenuItem;
 import android.widget.FrameLayout;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
